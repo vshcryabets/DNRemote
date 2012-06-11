bd96dc5fb57ac1e3cc932b0b73377b425c39645a
Vladimir Shcriabets <vshcryabets@gmail.com>
2012-06-11 15:12:20 +0300

    - license blocks for Android client

--- Android/src/com/v2soft/dnremote/ui/fragments/DesktopsEditorFragment.java
+++ Android/src/com/v2soft/dnremote/ui/fragments/DesktopsEditorFragment.java
@@ -1,5 +1,22 @@
+// Copyright (C) 2012 V.Shcryabets (vshcryabets@gmail.com)
+//
+// Author:  Vladimir Shcryabets <vshcryabets@gmail.com>
+//
+// This file is part of DNAndroidClient.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of version 3 of the GNU General 
+// Public License as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public
+// License along with this program; if not, write to the
+// Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+// Boston, MA 02111-1307, USA.
 package com.v2soft.dnremote.ui.fragments;
-
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
