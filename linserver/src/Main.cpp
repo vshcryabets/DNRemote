72b4e0c7fae7fd00bf4635c2a2ad781236942c30
Vladimir Shcriabets <vshcryabets@gmail.com>
2012-06-11 15:17:00 +0300

    - license blocks for server

--- linserver/src/Main.cpp
+++ linserver/src/Main.cpp
@@ -1,8 +1,23 @@
 /*
  * Main.cpp
+ * Created on: May 20, 2012
  *
- *  Created on: May 20, 2012
- *      Author: mrco
+ * Copyright (C) 2012 V.Shcryabets (vshcryabets@gmail.com)
+ * Author:  Vladimir Shcryabets <vshcryabets@gmail.com>
+ * This file is part of DNServer.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 3 of the GNU General
+ * Public License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
  */
 #include <string>
 #include "StyxServerManager.h"
